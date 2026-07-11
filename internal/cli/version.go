c6180c6670468d6e9cb4136532d8f1e5c09f9f91
Harrison-Blair <hjblair@me.com>
2026-07-11 03:03:34 -0400

    Release v0.5.4 (clean 4-platform release)

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -7,7 +7,7 @@ import (
 
 // binaryVersion is the version of the fledge binary itself.
 // Overridable at build time: -ldflags "-X ...internal/cli.binaryVersion=x.y.z".
-var binaryVersion = "0.5.3"
+var binaryVersion = "0.5.4"
 
 func init() { register("version", runVersion, "fledge version [--json]") }
 
