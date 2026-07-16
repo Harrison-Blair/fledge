154510fc963e7071b2f09297ecfeba2b6710e85e
Harrison-Blair <hjblair@me.com>
2026-07-15 23:57:27 -0400

    Bump version to 0.5.8; gitignore .fledge/roster/ in scaffold

--- internal/cli/init.go
+++ internal/cli/init.go
@@ -27,7 +27,7 @@ func init() {
 var defaultScanIgnore []byte
 
 // gitignore lines fledge needs; appended as one block when any is missing.
-var gitignoreLines = []string{".fledge/nest/raw/", ".fledge/broods/", ".alloc.lock"}
+var gitignoreLines = []string{".fledge/nest/raw/", ".fledge/broods/", ".fledge/roster/", ".alloc.lock"}
 
 // stringListFlag implements flag.Value for a repeatable, comma-separated list.
 type stringListFlag []string
